c16f837726974a785e0f9bfddc9fd54dc59f7345
DailyDreaming <lblauvel@ucsc.edu>
2020-04-08 01:19:59 -0700

    Add terra dev drs fetch.

--- test/test_basic_submission.py
+++ test/test_basic_submission.py
@@ -6,6 +6,7 @@ import unittest
 import os
 import json
 
+import google.cloud.storage
 import firecloud.api
 from gen3.submission import Gen3Submission
 from gen3.auth import Gen3Auth
@@ -13,10 +14,10 @@ from gen3.auth import Gen3Auth
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from test.utils import fetch_google_secret
+from test.utils import fetch_google_secret, fetch_drs_info, md5sum
 
 logger = logging.getLogger(__name__)
-
+gs_prefix = 'gs://'
 
 class TestGen3DataAccess(unittest.TestCase):
     @classmethod
@@ -26,6 +27,62 @@ class TestGen3DataAccess(unittest.TestCase):
             cls.gen3_key = json.loads(f.read())
         cls.gen3_auth_client = Gen3Auth(endpoint=cls.gen3_endpoint, refresh_token=cls.gen3_key)
         cls.gen3_sub_client = Gen3Submission(endpoint=cls.gen3_endpoint, auth_provider=cls.gen3_auth_client)
+        cls.google_storage_client = google.cloud.storage.Client()
+
+    def test_drs_in_terra(self):
+        # TODO: Don't use dev
+        # TODO: Grab the drs url from gen3
+        # TODO: Actually download the file and match the checksums
+        json_response = fetch_drs_info(drs_url="drs://dg.712C/fa640b0e-9779-452f-99a6-16d833d15bd0")
+
+        # Response should be:
+
+        # {'dos': {'data_object': {'aliases': [],
+        #                          'checksums': [{'checksum': '8bec761c8a626356eb34dbdfe20649b4', 'type': 'md5'}],
+        #                          'created': '2020-01-15T17:46:25.694142',
+        #                          'description': '',
+        #                          'id': 'dg.712C/fa640b0e-9779-452f-99a6-16d833d15bd0',
+        #                          'mime_type': '',
+        #                          'name': None,
+        #                          'size': 1386553,
+        #                          'updated': '2020-01-15T17:46:25.694148',
+        #                          'urls':
+        #                          [{'url': 'gs://fc-56ac46ea-efc4-4683-b6d5-6d95bed41c5e/CCDG_13607/Project_CCDG_13607_B01_GRM_WGS.cram.2019-02-06/Sample_HG01131/analysis/HG01131.final.cram.crai'}],
+        #                          'version': 'd87455aa'}}}
+
+        google_url_paths = [url['url'] for url in json_response['dos']['data_object']['urls'] if url['url'].startswith(gs_prefix)]
+
+        assert len(google_url_paths) == 1
+        google_url_path = google_url_paths[0]
+
+        bucket_name = google_url_path[len(gs_prefix):].split('/')[0]
+
+        bucket = self.google_storage_client.bucket(bucket_name, user_project=os.environ['GOOGLE_PROJECT_ID'])
+        blob_key = google_url_path[len(f'{gs_prefix}{bucket}/'):]
+        blob = bucket.blob(blob_key)
+        # print(blob)
+        download_path = os.path.join(pkg_root, 'test', google_url_path.split('/')[-1])
+
+        # # If :attr:`user_project` is set on the bucket, bills the API request to that project... otherwise:
+        # # BadRequestException: 400 Bucket is requester pays bucket but no user project provided.
+        # blob.download_to_filename(filename=download_path)
+
+        # Even still we're not allowed >.> :
+        # quokka@qcore:~$ gsutil -u platform-dev-178517 cp gs://fc-56ac46ea-efc4-4683-b6d5-6d95bed41c5e/CCDG_13607/Project_CCDG_13607_B01_GRM_WGS.cram.2019-02-06/Sample_HG01131/analysis/HG01131.final.cram.crai .
+        # AccessDeniedException: 403 biodata.integration.test.mule@gmail.com does not have serviceusage.services.use access to project 513406676236.
+
+        # TODO: Add the test account to platform-dev for requester pays billing
+
+        # expected_md5sums = [checksum for checksum in json_response['dos']['data_object']['checksums'] if checksum['type'] == 'md5']
+        # assert len(expected_md5sums) == 1
+        # expected_md5sum = expected_md5sums[0]
+        #
+        # assert md5sum(download_path) == expected_md5sum
+        # print(md5sum(download_path))
+        #
+        # print("Terra download finished: {download_path}")
+
+
 
     def test_data_introspect(self):
         response = self.gen3_sub_client.get_programs()  # {'links': ['/v0/submission/parent', '/v0/submission/topmed', '/v0/submission/open_access', '/v0/submission/tutorial']}
--- test/utils.py
+++ test/utils.py
@@ -1,4 +1,15 @@
 import os
+import json
+import requests
+import hashlib
+
+
+def md5sum(file_name):
+    hash = hashlib.md5()
+    with open(file_name, 'rb') as f:
+        for block in iter(lambda: f.read(size=100000), b''):
+            hash.update(block)
+    return hash.hexdigest()
 
 
 def fetch_google_secret(secret):
@@ -6,8 +17,24 @@ def fetch_google_secret(secret):
 
     client = SecretManagerServiceClient()
     try:
-        project = os.environ["GOOGLE_PROJECT_ID"]
+        project = os.environ["GOOGLE_PROJECT_NAME"]
     except KeyError:
-        raise RuntimeError('GOOGLE_PROJECT_ID is unset.  Please set GOOGLE_PROJECT_ID.')
+        raise RuntimeError('GOOGLE_PROJECT_NAME is unset.  Please set GOOGLE_PROJECT_ID.')
     response = client.access_secret_version(f'projects/{project}/secrets/{secret}/versions/latest')
     return response.payload.data.decode('UTF-8')
+
+
+# TODO: Amend terra-notebook-utils function to make generic
+#  https://github.com/DataBiosphere/terra-notebook-utils/blob/a97f4dbbe92725e0abf31405e8f1b1b6ede6113d/terra_notebook_utils/drs.py#L17-L30
+def fetch_drs_info(drs_url):
+    martha_dev_url = "https://us-central1-broad-dsde-dev.cloudfunctions.net/martha_v2"
+    headers = {
+        # 'authorization': f"Bearer {access_token}",  # not used in dev
+        'content-type': "application/json"
+    }
+    resp = requests.post(martha_dev_url, headers=headers, data=json.dumps(dict(url=drs_url)))
+    if 200 == resp.status_code:
+        return resp.json()
+    else:
+        print(resp.content)
+        resp.raise_for_status()
