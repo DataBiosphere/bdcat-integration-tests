2d8863a02c64e014f301e03978bdd54882cdf59b
DailyDreaming <lblauvel@ucsc.edu>
2020-04-23 09:54:03 -0700

    Generalize trigger script.

--- scripts/run_integration_tests.py
+++ scripts/run_integration_tests.py
@@ -1,20 +1,28 @@
 import requests
 import time
+import argparse
 
 PRIVATE_TOKEN = os.environ['BDCAT_PRIVATE_TEST_TOKEN']
 TOKEN = os.environ['BDCAT_TEST_TOKEN']
-BRANCH = 'master'
-ENDPOINT = 'https://biodata-integration-tests.net'
+DEFAULT_HOST = 'https://biodata-integration-tests.net'
+DEFAULT_BRANCH = 'master'
+DEFAULT_PROJECT_NUM = 3
 
-def main():
-    response = requests.post(f'{ENDPOINT}/api/v4/projects/3/trigger/pipeline?token={TOKEN}&ref={BRANCH}')
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='Gitlab Test Trigger')
+    parser.add_argument("--project", type=int, default=DEFAULT_PROJECT_NUM)
+    parser.add_argument("--branch", default=DEFAULT_BRANCH)
+    parser.add_argument("--host", default=DEFAULT_HOST)
+    o = parser.parse_args(argv)
+
+    response = requests.post(f'{o.host}/api/v4/projects/{o.project}/trigger/pipeline?token={TOKEN}&ref={o.branch}')
     response.raise_for_status()
-    PIPELINE = response.json()['web_url'].split('/')[-1].strip()
+    pipeline = response.json()['web_url'].split('/')[-1].strip()
     status = 'pending'
 
     while status in ('pending', 'running'):
         time.sleep(10)
-        response = requests.get(f'{ENDPOINT}/api/v4/projects/3/pipelines/{PIPELINE}',
+        response = requests.get(f'{o.host}/api/v4/projects/{o.project}/pipelines/{pipeline}',
                                 headers={'PRIVATE-TOKEN': PRIVATE_TOKEN})
         status = response.json()['status']
 
