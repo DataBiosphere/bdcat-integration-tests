78badf16a2a1c080f55d7788ed4dbb70bf2777a5
DailyDreaming <lblauvel@ucsc.edu>
2020-05-26 13:05:10 -0400

    Make trigger python 2/3 compatible.

--- scripts/run_integration_tests.py
+++ scripts/run_integration_tests.py
@@ -16,30 +16,40 @@ def main(argv=sys.argv[1:]):
     parser.add_argument("--project", type=int, default=DEFAULT_PROJECT_NUM)
     parser.add_argument("--branch", default=DEFAULT_BRANCH)
     parser.add_argument("--host", default=DEFAULT_HOST)
+    parser.add_argument("--quiet", default=False, help='Only print the final status message or error messages.')
     o = parser.parse_args(argv)
 
-    response = requests.post(f'{o.host}/api/v4/projects/{o.project}/trigger/pipeline?token={TOKEN}&ref={o.branch}')
+    job_trigger_url = o.host + '/api/v4/projects/' + str(o.project) + '/trigger/pipeline?token=' + TOKEN + '&ref=' + o.branch
+
+    response = requests.post(job_trigger_url)
     response.raise_for_status()
     test_url = response.json()['web_url']
     pipeline = test_url.split('/')[-1].strip()
     status = 'pending'
 
-    print(f'Starting integration tests.  Checking status in 10 seconds.\n'
-          f'See: {test_url}')
+    if not o.quiet:
+        print('Starting integration tests.  Checking status in 10 seconds.\n'
+              'See: ' + test_url)
+
+    job_status_url = o.host + '/api/v4/projects/' + str(o.project) + '/pipelines/' + pipeline
     while status in ('pending', 'running'):
         time.sleep(10)
-        response = requests.get(f'{o.host}/api/v4/projects/{o.project}/pipelines/{pipeline}',
+        response = requests.get(job_status_url,
                                 headers={'PRIVATE-TOKEN': PRIVATE_TOKEN})
         try:
             status = response.json()['status']
-            print(f'Status is: {status}.  Checking status again in 10 seconds.')
+            if not o.quiet:
+                print('Status is: ' + status)
+                print('Checking status again in 10 seconds.')
         except:
             print(response.content)
-            exit(0)
+            raise
 
     if status == 'failed':
-        raise RuntimeError(f'Integration Tests have Failed: {test_url}')
-    print(f'Exiting.  Status was: {status}.  See: {test_url}')
+        raise RuntimeError('Integration Tests have Failed: ' + test_url)
+
+    print('Exiting.  Status was: ' + status)
+    print('See: ' + test_url)
 
 
 if __name__ == '__main__':
