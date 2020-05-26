ad2887b5bfc9f3ae7240e8fbf65b5eca57922057
DailyDreaming <lblauvel@ucsc.edu>
2020-05-26 13:23:04 -0400

    Address review comments.

--- scripts/run_integration_tests.py
+++ scripts/run_integration_tests.py
@@ -16,10 +16,10 @@ def main(argv=sys.argv[1:]):
     parser.add_argument("--project", type=int, default=DEFAULT_PROJECT_NUM)
     parser.add_argument("--branch", default=DEFAULT_BRANCH)
     parser.add_argument("--host", default=DEFAULT_HOST)
-    parser.add_argument("--quiet", default=False, help='Only print the final status message or error messages.')
-    o = parser.parse_args(argv)
+    parser.add_argument("--quiet", default=False, help='Suppress printing run messages.')
+    args = parser.parse_args(argv)
 
-    job_trigger_url = o.host + '/api/v4/projects/' + str(o.project) + '/trigger/pipeline?token=' + TOKEN + '&ref=' + o.branch
+    job_trigger_url = args.host + '/api/v4/projects/' + str(args.project) + '/trigger/pipeline?token=' + TOKEN + '&ref=' + args.branch
 
     response = requests.post(job_trigger_url)
     response.raise_for_status()
@@ -27,29 +27,27 @@ def main(argv=sys.argv[1:]):
     pipeline = test_url.split('/')[-1].strip()
     status = 'pending'
 
-    if not o.quiet:
+    if not args.quiet:
         print('Starting integration tests.  Checking status in 10 seconds.\n'
               'See: ' + test_url)
 
-    job_status_url = o.host + '/api/v4/projects/' + str(o.project) + '/pipelines/' + pipeline
+    job_status_url = args.host + '/api/v4/projects/' + str(args.project) + '/pipelines/' + pipeline
     while status in ('pending', 'running'):
         time.sleep(10)
         response = requests.get(job_status_url,
                                 headers={'PRIVATE-TOKEN': PRIVATE_TOKEN})
-        try:
-            status = response.json()['status']
-            if not o.quiet:
-                print('Status is: ' + status)
-                print('Checking status again in 10 seconds.')
-        except:
-            print(response.content)
-            raise
+        response.raise_for_status()
+        status = response.json()['status']
+        if not args.quiet:
+            print('Status is: ' + status)
+            print('Checking status again in 10 seconds.')
 
     if status == 'failed':
         raise RuntimeError('Integration Tests have Failed: ' + test_url)
 
-    print('Exiting.  Status was: ' + status)
-    print('See: ' + test_url)
+    if not args.quiet:
+        print('Exiting.  Status was: ' + status)
+        print('See: ' + test_url)
 
 
 if __name__ == '__main__':
