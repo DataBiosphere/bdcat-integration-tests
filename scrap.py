1990945a947ef7a2e294049bd304e6d680a826f3
DailyDreaming <lblauvel@ucsc.edu>
2020-08-26 17:42:46 -0400

    Cruft.

--- scrap.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-# sudo rm -rf v3nv && virtualenv -p python3.8 v3nv && . v3nv/bin/activate && pip install -r requirements.txt && python scrap.py
-
-import google.cloud.storage
-
-print('hello')
