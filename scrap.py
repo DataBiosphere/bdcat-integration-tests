7c8a5ea67e72b29df410a91383bca9905f6216e7
DailyDreaming <lblauvel@ucsc.edu>
2020-08-26 16:36:58 -0400

    Test file.

---
+++ scrap.py
@@ -0,0 +1,9 @@
+
+
+
+
+# sudo rm -rf v3nv && virtualenv -p python3.8 v3nv && . v3nv/bin/activate && pip install -r requirements.txt && python scrap.py
+
+import google.cloud.storage
+
+print('hello')
