32721ecf70dec71849575a6c910782bdce383965
DailyDreaming <lblauvel@ucsc.edu>
2020-09-28 16:39:22 -0400

    Truncate again.

--- scripts/post_to_slack.py
+++ scripts/post_to_slack.py
@@ -6,6 +6,8 @@ import sys
 import argparse
 import json
 
+from threading import Thread
+
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
@@ -18,33 +20,33 @@ GITLAB_USER_NAME = os.environ['GITLAB_USER_NAME']
 CI_JOB_URL = os.environ['CI_JOB_URL']
 CI_PIPELINE_URL = os.environ['CI_PIPELINE_URL']
 
+def post_notification(host, project):
+    slack_notification_url = 'https://hooks.slack.com/services/' + SLACK_WEBHOOK
+    headers = {
+        'Content-type': 'application/json'
+    }
+    status = wait_for_final_status(pipeline=CI_PIPELINE_URL.split('/')[-1],
+                                   host=host,
+                                   project=project,
+                                   quiet=True)
+    data = {
+        'text': f'{GITLAB_USER_NAME} triggered: <{CI_JOB_URL}>\n'
+                f'Status is: {status}'
+    }
+    response = requests.post(slack_notification_url, data=json.dumps(data), headers=headers)
+    response.raise_for_status()
+
 
 def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser(description='Post integration testing status to slack.')
     parser.add_argument("--project", type=int, default=DEFAULT_PROJECT_NUM)
     parser.add_argument("--branch", default=DEFAULT_BRANCH)
     parser.add_argument("--host", default=DEFAULT_HOST)
-    parser.add_argument("--quiet", default=True, help='Suppress printing run messages.')
     args = parser.parse_args(argv)
 
-    slack_notification_url = 'https://hooks.slack.com/services/' + SLACK_WEBHOOK
-    headers = {
-        'Content-type': 'application/json'
-    }
-    status = wait_for_final_status(pipeline=CI_PIPELINE_URL.split('/')[-1],
-                                   host=args.host,
-                                   project=args.project,
-                                   quiet=args.quiet)
-    data = {
-        'text': f'{GITLAB_USER_NAME} triggered: <{CI_JOB_URL}>\n'
-                f'Status is: {status}'
-    }
-
-    response = requests.post(slack_notification_url, data=json.dumps(data), headers=headers)
-    response.raise_for_status()
-
-    if not args.quiet:
-        print(response.json())
+    t = Thread(target=post_notification, host=args.host, project=args.project)
+    t.daemon = True
+    t.start()
 
 
 if __name__ == '__main__':
