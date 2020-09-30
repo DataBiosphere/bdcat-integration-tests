f6b209b1b9528503ee04b5aaa12dfe99d7bc730f
DailyDreaming <lblauvel@ucsc.edu>
2020-09-30 15:48:19 -0400

    Extend poll time.

--- scripts/post_to_slack.py
+++ scripts/post_to_slack.py
@@ -40,7 +40,7 @@ def post_notification(host, project):
                                    host=host,
                                    project=project,
                                    quiet=True,
-                                   interval=0.2)
+                                   interval=0.5)
     data = {
         'text': f'{GITLAB_USER_NAME} triggered: <{CI_JOB_URL}>\n'
                 f'Status is: {status}'
