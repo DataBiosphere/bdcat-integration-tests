7d000e56a86cd1956c8e34f928fa75efe50baacf
Lon Blauvelt <lblauvel@ucsc.edu>
2021-03-15 22:25:30 -0700

    Add prod domains. (#52)
    * Add prod domains.
    
    * Add prod yml file.
    
    * Clean up.
    
    * Add prod yml file.

--- test/test_basic_submission.py
+++ test/test_basic_submission.py
@@ -13,6 +13,7 @@ import warnings
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
+from test.infra.testmode import staging_only
 from test.utils import (run_workflow,
                         create_terra_workspace,
                         delete_terra_workspace,
@@ -24,7 +25,8 @@ from test.utils import (run_workflow,
                         check_workflow_presence_in_terra_workspace,
                         delete_workflow_presence_in_terra_workspace,
                         check_workflow_status,
-                        import_drs_from_gen3)
+                        import_drs_from_gen3,
+                        STAGE)
 
 logger = logging.getLogger(__name__)
 
@@ -47,7 +49,7 @@ class TestGen3DataAccess(unittest.TestCase):
                         os.path.expanduser('~/.config/gcloud/application_default_credentials.json'))
         except shutil.SameFileError:
             pass
-        print(f'Terra Health Status:\n\n{json.dumps(check_terra_health(), indent=4)}')
+        print(f'Terra [{STAGE}] Health Status:\n\n{json.dumps(check_terra_health(), indent=4)}')
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -104,6 +106,7 @@ class TestGen3DataAccess(unittest.TestCase):
         with self.subTest('Dockstore Check Workflow Not Seen'):
             self.assertFalse(wf_seen_in_terra)
 
+    @staging_only
     def test_drs_workflow_in_terra(self):
         """This test runs md5sum in a fixed workspace using a drs url from gen3."""
         response = run_workflow()
@@ -163,6 +166,7 @@ class TestGen3DataAccess(unittest.TestCase):
             response = delete_terra_workspace(workspace=workspace_name)
             self.assertTrue(response.status_code == 404)
 
+    @staging_only
     def test_import_drs_from_gen3(self):
         # file is ~1gb, so only download the first byte to check for access
         import_drs_from_gen3('drs://dg.712C/95dc0845-d895-489f-aaf8-583a676037f7')
