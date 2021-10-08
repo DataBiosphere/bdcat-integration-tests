87dea338bce7ae2faa09251c02362339b825becb
Nicole Bolliger <bolliger@broadinstitute.org>
2021-10-08 12:44:29 -0400

    importing all test.utils

--- test/test_performance.py
+++ test/test_performance.py
@@ -6,14 +6,10 @@ import time
 import datetime
 import os
 import sys
+import test.utils
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
-
-from test.utils import (create_terra_workspace,
-                        delete_terra_workspace,
-                        pfb_job_status_in_terra,
-                        import_pfb)
 
 logger = logging.getLogger(__name__)
 
@@ -27,32 +23,32 @@ class TestPerformance(unittest.TestCase):
         workspace_name = f'drs_test_{time_stamp}_delete_me'
 
         with self.subTest('Create a terra workspace.'):
-            response = create_terra_workspace(workspace=workspace_name)
+            response = test.utils.create_terra_workspace(workspace=workspace_name)
             self.assertTrue('workspaceId' in response)
             self.assertTrue(response['createdBy'] == 'biodata.integration.test.mule@gmail.com')
 
         with self.subTest('Import static pfb into the terra workspace.'):
-            response = import_pfb(workspace=workspace_name, pfb_file=large_pfb)
+            response = test.utils.import_pfb(workspace=workspace_name, pfb_file=large_pfb)
             self.assertTrue('jobId' in response)
 
         with self.subTest('Check on the import static pfb job status.'):
-            response = pfb_job_status_in_terra(workspace=workspace_name, job_id=response['jobId'])
+            response = test.utils.pfb_job_status_in_terra(workspace=workspace_name, job_id=response['jobId'])
             # this should take < ??????????
             while response['status'] in ['Translating', 'ReadyForUpsert', 'Upserting', 'Pending']:
                 time.sleep(2)
-                response = pfb_job_status_in_terra(workspace=workspace_name, job_id=response['jobId'])
+                response = test.utils.pfb_job_status_in_terra(workspace=workspace_name, job_id=response['jobId'])
             self.assertTrue(response['status'] == 'Done',
                             msg=f'Expecting status: "Done" but got "{response["status"]}".\n'
                                 f'Full response: {json.dumps(response, indent=4)}')
 
         with self.subTest('Delete the terra workspace.'):
-            response = delete_terra_workspace(workspace=workspace_name)
+            response = test.utils.delete_terra_workspace(workspace=workspace_name)
             if not response.ok:
                 raise RuntimeError(
                     f'Could not delete the workspace "{workspace_name}": [{response.status_code}] {response}')
             if response.status_code != 202:
                 logger.critical(f'Response {response.status_code} has changed: {response}')
-            response = delete_terra_workspace(workspace=workspace_name)
+            response = test.utils.delete_terra_workspace(workspace=workspace_name)
             self.assertTrue(response.status_code == 404)
 
 
